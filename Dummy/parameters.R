f42a6a00e8c77d8ab00c0d4ed42a557a73756dbd
baj12 <bernd.jagla@pasteur.fr>
2019-04-03 22:30:15 +0200

    log

--- Dummy/parameters.R
+++ Dummy/parameters.R
@@ -3,18 +3,25 @@
 # normalization parameters
 #####
 
-
+# myNormalizationChoices ----
 # choice for the radio buttion
+# the name of the variable will be displayed, the value is the name of the reactive
 myNormalizationChoices <- list(
   Dummy_Normalization = "Dummy_Normalization"
 )
 
+# myNormalizationParameters ----
 # value should be of class shiny.tag
 # will be displayed via renderUI
 myNormalizationParameters <- list(
   Dummy_Normalization = h4("no Parameters implemented")
 )
 
+# Dummy_Normalization ----
+#' Dummy_Normalization
+#' here, we calculate the normalization
+#' the result has to be a singleCellExperiment object with the slot logcounts
+#' In the example below we devide 
 Dummy_Normalization <- reactive({
   # track how much time is spent here
   start.time <- base::Sys.time()
@@ -46,6 +53,18 @@ Dummy_Normalization <- reactive({
   }
   # load(file="~/scShinyHubDebug/Dummy_Normalization.RData")
   
+  
+# normalization process
+  retVal <- Dummy_NormalizationFunc(scEx = scEx)
+# end of normalization. 
+   
+  printTimeEnd(start.time, "Dummy_Normalization")
+  exportTestValues(Dummy_Normalization = {assays(retVal)[["logcounts"]]})  
+  return(retVal)
+})
+
+
+Dummy_NormalizationFunc <- function(scEx){
   bc_sums <- Matrix::colSums((assays(scEx)[["counts"]]>0)*1)
   A <- as(assays(scEx)[["counts"]], "dgCMatrix")
   A@x <- A@x / bc_sums[assays(scEx)[["counts"]]@j + 1L]
@@ -56,8 +75,5 @@ Dummy_Normalization <- reactive({
   x <- uniqTsparse(assays(scEx_bcnorm)[[1]])
   slot(x, "x") <- slot(x, "x") * scalingFactor
   assays(scEx_bcnorm)[[1]] <- x
-  
-  printTimeEnd(start.time, "Dummy_Normalization")
-  exportTestValues(Dummy_Normalization = {scEx_bcnorm})  
   return(scEx_bcnorm)
-})
+}
