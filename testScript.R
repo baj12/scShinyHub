4d7e4134e40701cec80f0c13c4d4be8b15621279
baj12 <bernd.jagla@pasteur.fr>
2019-04-11 11:57:58 +0200

    tests

--- testScript.R
+++ testScript.R
@@ -4,5 +4,7 @@ recordTest(".")
 
 system.time(testApp(".", "mytest.R",compareImages = TRUE))
 system.time(testApp(".", "mytest2.R",compareImages = TRUE))
+system.time(testApp(".", "mytest3.R",compareImages = TRUE))
+system.time(testApp(".", "mytestGQC.R",compareImages = TRUE))
 
-snapshotUpdate(".", "mytest2")
+snapshotUpdate(".", "mytest3")
--- tests/mytest3.R
+++ tests/mytest3.R
@@ -1,10 +1,29 @@
+defaultTimeout = 20000
+defaultWait = 10
+snNr = 1
+
+
+
 app <- ShinyDriver$new("../")
 app$snapshotInit("mytest3")
+Sys.sleep(5)
 
 app$uploadFile(file1 = "../scEx.Rds", wait_=FALSE, values_=FALSE) # <-- This should be the path to the file, relative to the app's tests/ directory
-app$setInputs(sidebarCollapsed = FALSE, wait_=FALSE, values_=FALSE)
-Sys.sleep(240)
+Sys.sleep(20)
+cat(file = stderr(), paste("snapshop", snNr, "\n")); snNr=snNr+1;app$snapshot()
+
+app$setInputs(sidebarItemExpanded = "Co-expression", wait_=FALSE, values_=FALSE)
+app$setInputs(sideBarID = "coexpressionSelected", wait_=FALSE, values_=FALSE)
+app$setInputs(`coE_heatmapSelectedModule-pHeatMapPlot__shinyjquiBookmarkState__resizable` = c(574, 400), allowInputNoBinding_ = TRUE, wait_=FALSE, values_=FALSE)
+app$setInputs(`coE_heatmapSelectedModule-pHeatMapPlot_size` = c(574, 400), allowInputNoBinding_ = TRUE, wait_=FALSE, values_=FALSE)
+app$setInputs(`coE_heatmapSelectedModule-pHeatMapPlot_is_resizing` = FALSE, allowInputNoBinding_ = TRUE, wait_=FALSE, values_=FALSE)
+app$setInputs(`coE_selected-clusterPlot__shinyjquiBookmarkState__resizable` = c(574, 400), allowInputNoBinding_ = TRUE, wait_=FALSE, values_=FALSE)
+app$setInputs(`coE_selected-clusterPlot_size` = c(574, 400), allowInputNoBinding_ = TRUE, wait_=FALSE, values_=FALSE)
+app$setInputs(`coE_selected-clusterPlot_is_resizing` = FALSE, allowInputNoBinding_ = TRUE, wait_=FALSE, values_=FALSE), wait_=FALSE, values_=FALSE)
+app$setInputs(sidebarItemExpanded = "GeneralQC", wait_=FALSE, values_=FALSE), wait_=FALSE, values_=FALSE)
+app$setInputs(sideBarID = "gQC_sampleHist", wait_=FALSE, values_=FALSE)
 app$snapshot()
-app$setInputs(goCalc = "click", wait_=FALSE, values_=FALSE)
-Sys.sleep(600)
+cat(file = stderr(), paste("snapshop", snNr, "\n")); snNr=snNr+1;app$snapshot()
+app$setInputs(sideBarID = "gQC_variancePC", wait_=FALSE, values_=FALSE)
 app$snapshot()
+cat(file = stderr(), paste("snapshop", snNr, "\n")); snNr=snNr+1;app$snapshot()
